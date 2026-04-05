d0c9afb93aab94627fe3b25f4658b0e964fc4177
Raissa Cavalcanti <154569377+cavalcantiraissas@users.noreply.github.com>
2026-04-04 23:44:16 -0300

    Create dados.js

---
+++ Programacao-Web/atividades-classe/gestao-de-inventario/dados.js
@@ -0,0 +1,31 @@
+export const inventarioLoja = [
+    {
+      id: 1,
+      nome: "Notebook Gamer",
+      preco: 4500.00,
+      emEstoque: true,
+      quantidade: 5
+    },
+    {
+      id: 2,
+      nome: "Mouse sem Fio",
+      preco: 120.50,
+      emEstoque: true,
+      quantidade: 25
+    },
+    {
+      id: 3,
+      nome: "Teclado Mecânico",
+      preco: 350.00,
+      emEstoque: false,
+      quantidade: 0
+    },
+    {
+      id: 4,
+      nome: "Monitor 27\"",
+      preco: 1890.00,
+      emEstoque: true,
+      quantidade: 7
+    }
+  ];
+  
