d6015a92f767be7c4a798e1f8ab6697733a99fad
cavalcantiraissas <cavalcanti.c.raissa@gmail.com>
2026-09-20 00:27:15 -0300

    Corrige nome da função insereFilaEnc (estava com typo e não compilava)

--- Estrutura_de_Dados_C/Atividades_de_Classe/Fila_Alocacao-Dinamica-Encadeada/main.c
+++ Estrutura_de_Dados_C/Atividades_de_Classe/Fila_Alocacao-Dinamica-Encadeada/main.c
@@ -7,12 +7,12 @@ int main(int argc, char *argv[]) {
     tipo_no *minha_fila;
     minha_fila =NULL;
 
-    inserreFilaEnc(&minha_fila, 7);
-    inserreFilaEnc(&minha_fila, 1);
-    inserreFilaEnc(&minha_fila, 21);
-    inserreFilaEnc(&minha_fila, 3);
-    inserreFilaEnc(&minha_fila, 9);
-    inserreFilaEnc(&minha_fila, 10);
+    insereFilaEnc(&minha_fila, 7);
+    insereFilaEnc(&minha_fila, 1);
+    insereFilaEnc(&minha_fila, 21);
+    insereFilaEnc(&minha_fila, 3);
+    insereFilaEnc(&minha_fila, 9);
+    insereFilaEnc(&minha_fila, 10);
 
     printf("Valor removido = %d\n", removeFilaEnc(&minha_fila));
 
